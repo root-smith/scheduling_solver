9ea50de29ecfc3aba02f6931035983ead7770d8a
root-smith <kate.ruter@gmail.com>
2018-04-18 19:20:33 -0500

    Initial Commit

---
+++ scheduling_solver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  scheduling_solver
+//
+//  Created by Kate Ruter on 4/18/18.
+//  Copyright © 2018 Kate Rutersmith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
